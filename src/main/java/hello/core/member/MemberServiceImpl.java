7537d5ce97d312410e070dbcd71aa4762eb06853
DevelopJKong <ybaram40@gmail.com>
2021-07-18 15:44:27 +0900

    Initial commit

---
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,16 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepositroy();
+    //추상화에도 의존하고 구체화에도 의존하고 있다
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
